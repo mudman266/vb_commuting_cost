f1bda0321bf87435fdfe4203c4f4a20d8bde2f92
Josh Williams <josh@jwillcreations.com>
2020-12-19 16:56:10 -0500

    Adding existing files

---
+++ Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.picBackground = New System.Windows.Forms.PictureBox()
+        CType(Me.picBackground, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBackground
+        '
+        Me.picBackground.Image = CType(resources.GetObject("picBackground.Image"), System.Drawing.Image)
+        Me.picBackground.Location = New System.Drawing.Point(12, 12)
+        Me.picBackground.Name = "picBackground"
+        Me.picBackground.Size = New System.Drawing.Size(356, 223)
+        Me.picBackground.TabIndex = 0
+        Me.picBackground.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(813, 451)
+        Me.Controls.Add(Me.picBackground)
+        Me.Name = "frmMain"
+        Me.Text = "Commuting Cost"
+        CType(Me.picBackground, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picBackground As PictureBox
+End Class
